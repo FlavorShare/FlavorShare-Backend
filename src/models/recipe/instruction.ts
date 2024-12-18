a6b0d4152716b2821d03bcfa4b32ad0a7388cc30
Benjamin Lefebvre <benjamin.lefebvre.1024@gmail.com>
2024-12-18 15:24:33 -0500

    Fix instruction not ID set

--- src/models/recipe/instruction.ts
+++ src/models/recipe/instruction.ts
@@ -2,14 +2,12 @@ import mongoose, { Schema } from "mongoose";
 import { Ingredient, ingredientSchema } from "./ingredient";
 
 export interface Instruction {
-  _id: string;
   step: number;
   description: string;
   ingredients?: String[];
 }
 
 export const instructionSchema = new Schema<Instruction>({
-  _id: { type: String, required: true },
   step: { type: Number, required: true },
   description: { type: String, required: true },
   ingredients: { type: [String] },
