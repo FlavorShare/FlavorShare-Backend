4a56be0dc43332c8dbeb342b3f4bba3d991ebb81
Benjamin Lefebvre <benjamin.lefebvre.1024@gmail.com>
2025-07-22 13:53:36 -0400

    temp remove google firebase

--- src/index.ts
+++ src/index.ts
@@ -16,25 +16,25 @@ import admin from "firebase-admin";
 // Load environment variables from .env file
 dotenv.config();
 
-// Parse the service account key from the environment variable
-const serviceAccountKey = JSON.parse(process.env.SERVICE_ACCOUNT_KEY as string);
+// // Parse the service account key from the environment variable
+// const serviceAccountKey = JSON.parse(process.env.SERVICE_ACCOUNT_KEY as string);
 
-// Initialize Firebase Admin SDK
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccountKey),
-});
+// // Initialize Firebase Admin SDK
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccountKey),
+// });
 
-const firebaseConfig = {
-  apiKey: process.env.FB_API_KEY,
-  authDomain: process.env.FB_AUTH_DOMAIN,
-  projectId: process.env.FB_PROJECT_ID,
-  storageBucket: process.env.FB_STORAGE_BUCKET,
-  messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
-  appId: process.env.FB_APP_ID,
-  measurementId: process.env.FB_MEASUREMENT_ID,
-};
+// const firebaseConfig = {
+//   apiKey: process.env.FB_API_KEY,
+//   authDomain: process.env.FB_AUTH_DOMAIN,
+//   projectId: process.env.FB_PROJECT_ID,
+//   storageBucket: process.env.FB_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+//   appId: process.env.FB_APP_ID,
+//   measurementId: process.env.FB_MEASUREMENT_ID,
+// };
 
-const firebaseApp = initializeApp(firebaseConfig);
+// const firebaseApp = initializeApp(firebaseConfig);
 
 const app = express();
 const port = 3000;
